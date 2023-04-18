cdc60876f1199d48593ace85be1be9b46d4d4186
NSENGIYUMVA CHRISTIAN <cristiannsengi@gmail.com>
2023-04-18 14:15:34 +0200

    first commit

---
+++ src/data.js
@@ -0,0 +1,36 @@
+const reviews = [
+  {
+    id: 1,
+    name: "NSENGIYUMVA CHRISTIAN",
+    job: "web developer",
+    image:
+      "https://media.licdn.com/dms/image/D4D03AQFFov7JZu54Qg/profile-displayphoto-shrink_400_400/0/1667823935550?e=1687392000&v=beta&t=m0uR0XwMcWjlcIFIg6aIQuc2He8Uy9tEeMjEQiaXgtY",
+    text: "Aspiring to become a software developer, my passion lies in creating innovative solutions through coding. The idea of designing, developing, and implementing software programs that can simplify and enhance people's lives excites me. I am constantly learning and expanding my skills in various programming languages to stay up-to-date with the latest technologies. To me, software development is not just a career choice, but a journey of continuous learning and growth.",
+  },
+  {
+    id: 2,
+    name: "anna johnson",
+    job: "web designer",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+  },
+  {
+    id: 3,
+    name: "peter jones",
+    job: "intern",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+  },
+  {
+    id: 4,
+    name: "bill anderson",
+    job: "the boss",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+  },
+];
+
+export default reviews;
